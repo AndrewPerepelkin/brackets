0a16de6ab132f29f0605a5862d9d86dc6eae97d8
AndrewPerepelkin <avp.tep@rambler.ru>
2021-03-15 12:28:55 +0300

    init: added task answer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  let hasError = false;
+
+  const isOpenBracket = (char) => bracketsConfig.some((tuple) => tuple[0] === char);
+  const isCloseBracket = (char) => !isOpenBracket(char);
+  const isTuple = (char1, char2) => bracketsConfig.some((tuple) => tuple[0] === char1 && tuple[1] === char2);
+
+  const addToStack = (char) => {
+    if (stack.length > 0) {
+      const lastStackChar = stack[stack.length - 1];
+      if (isTuple(lastStackChar, char)) {
+        stack.pop();
+      } else if (isOpenBracket(char)) {
+        stack.push(char);
+      } else {
+        hasError = true;
+      }
+    } else if (isCloseBracket(char)) {
+      hasError = true;
+    } else {
+      stack.push(char);
+    }
+  };
+
+  str.split('').forEach((char) => {
+    addToStack(char);
+  });
+
+  if (hasError || (stack.length > 0)) {
+    return false;
+  }
+  return true;
+
 }
